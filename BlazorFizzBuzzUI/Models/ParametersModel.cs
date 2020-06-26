5523330e75a5cb91625930c29d0c6a7686dae4b2
BSchafer01 <brandon.schaf@gmail.com>
2020-06-26 13:30:14 -0700

    Implemented the UI for FizzBuzz
    Created the logic and initial UI for FizzBuzz in Blazor

---
+++ BlazorFizzBuzzUI/Models/ParametersModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorFizzBuzzUI.Models
+{
+    public class ParametersModel
+    {
+        [Required]
+        public int Start { get; set; }
+        [Required]
+        public int End { get; set; }
+        [Required]
+        public int Fizz { get; set; }
+        [Required]
+        public int Buzz { get; set; }
+
+    }
+}
---
+++ BlazorFizzBuzzUI/Models/ResultModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorFizzBuzzUI.Models
+{
+    public class ResultModel
+    {
+        public int Number { get; set; }
+        public int Fizz { get; set; }
+        public int Buzz { get; set; }
+        public string Result => result();
+
+        private string result()
+        {
+            string res = "";
+            if (Fizz == 0)
+            {
+                res = "Fizz";
+            }
+            if (Buzz == 0)
+            {
+                res += "Buzz";
+            }
+            if (Fizz != 0 && Buzz != 0)
+            {
+                res = Number.ToString();
+            }
+
+            return res;
+        }
+    }
+}
